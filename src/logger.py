961d2752c220bbd0124cad2456df79a2717732fa
MOHD RIYAN OZAIR <149253756+RiyanOzair@users.noreply.github.com>
2026-03-05 11:37:16 +0530

    Added data ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"log_{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}.log"
+LOG_FILE = f"log_{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.log"
 log_path = os.path.join(os.getcwd(), 'logs')
 os.makedirs(log_path, exist_ok=True)
 
