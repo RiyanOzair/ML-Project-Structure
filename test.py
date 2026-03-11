0193d12ae8805cc0566895c500f7981b28501bb2
MOHD RIYAN OZAIR <149253756+RiyanOzair@users.noreply.github.com>
2026-03-11 09:55:00 +0530

    Added health endpoint

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import requests
 
-url = "https://student-performance-predictor-ml-end2end.onrender.com"
+url = "https://your-app-name.onrender.com/predict_api"
 
 data = {
- "gender":"male",
- "race_ethnicity":"group A",
- "parental_level_of_education":"bachelor's degree",
- "lunch":"standard",
- "test_preparation_course":"none",
- "reading_score":72,
- "writing_score":74
+ "gender": "male",
+ "race_ethnicity": "group A",
+ "parental_level_of_education": "bachelor's degree",
+ "lunch": "standard",
+ "test_preparation_course": "none",
+ "reading_score": 72,
+ "writing_score": 74
 }
 
 response = requests.post(url, json=data)
